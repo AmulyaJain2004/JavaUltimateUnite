5860bbfdff231dd55e56fb594a9eb1c2659136f9
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-03-18 20:58:51 +0530

    Create _3_Type_casting.java
    Program to show data type casting in Java.

---
+++ Java_basics /_3_Type_casting.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    // Implicit casting (widening) 
+    int intValue = 10; 
+    double doubleValue = intValue;
+
+    System.out.println("Double Value: " + doubleValue);
+
+    // Explicit casting (narrowing)
+    double doubleVal = 20.5;
+    int intVal = (int) doubleVal;
+
+    System.out.println("Int Value: " + intVal);
+  }
+}
