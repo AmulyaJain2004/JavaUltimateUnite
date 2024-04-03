e668881188a18c81ee2ee3ff54cb65c857bf1e7e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-04-03 23:42:51 +0530

    Create _4_Conditional_statements.java
    Program to implement checking conditions using if else in Java.

---
+++ Java_basics /_4_Conditional_statements.java
@@ -0,0 +1,11 @@
+public class _4_Conditional_statements {
+  public static void main(String[] args) {
+    int number = 10;
+
+    if (number % 2 == 0) {
+        System.out.println("Number is even.");
+    } else {
+        System.out.println("Number is odd.");
+    }
+  }
+}
