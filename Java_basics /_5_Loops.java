2ed784965992f1e860bbc1931f44e09375f86405
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-04-03 23:46:06 +0530

    Create _5_Loops.java
    Program to implement basic loops that is for loop and while loop in Java.

---
+++ Java_basics /_5_Loops.java
@@ -0,0 +1,16 @@
+public class _5_Loops {
+  public static void main(String[] args) {
+    // While loop 
+    int i = 1; 
+    while (i <= 5) 
+    { 
+      System.out.println("While loop iteration: " + i); 
+      i++;
+    }
+
+    // For loop
+    for (int j = 1; j <= 5; j++) {
+        System.out.println("For loop iteration: " + j);
+    }
+  }
+}
